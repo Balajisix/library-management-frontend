ede00339164e6b834d640f0d08459edcb3fd0fbe
Balaji <balajivs0305@gmail.com>
2025-05-06 21:09:50 +0530

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000/api/books';
+// const API_URL = 'http://localhost:5000/api/books';
+const API_URL = 'https://library-backend-bice.vercel.app/api/books';
 
 async function fetchBooks() {
   try {
